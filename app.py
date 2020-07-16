d725bd51157670632634f571fbb6cd1955c25680
mwumigue <mwuliang@gmail.com>
2020-07-16 17:33:08 +0000

    MVP program

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
+from flask import render_template
+from flask import request
+import model
 # from flask import render_template
 # from flask import request
 
@@ -11,6 +14,34 @@ app = Flask(__name__)
 
 # -- Routes section --
 @app.route('/')
-@app.route('/index')
-def index():
-    return "hello world"
+@app.route('/favorite_color')
+def favorite_color():
+    return render_template("favorite_color.html")
+@app.route('/favorite_element', methods=['GET', 'POST'])
+def favorite_element():
+    user_color = request.form["user_color"]
+    if user_color.lower() == "red":
+        return render_template("favorite_element_red.html")
+    elif user_color.lower() == "blue":
+        return render_template("favorite_element_blue.html")
+    elif user_color.lower() == "green":
+        return render_template("favorite_element_green.html")
+    elif user_color.lower() == "yellow":
+        return render_template("favorite_element_yellow.html")
+@app.route('/favorite_element_red')
+def favorite_element_red():
+    return render_template("favorite_element_red.html")
+@app.route('/favorite_element_blue')
+def favorite_element_blue():
+    return render_template("favorite_element_blue.html")
+@app.route('/favorite_element_green')
+def favorite_element_green():
+    return render_template("favorite_element_green.html")
+@app.route('/favorite_element_yellow')
+def favorite_element_yellow():
+    return render_template("favorite_element_yellow.html")
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    user_element = request.form["user_element"]
+    user_pokemon = model.favorite_element(user_element)
+    return render_template("results.html", user_pokemon = user_pokemon)
---
+++ model.py
@@ -0,0 +1,24 @@
+red_elements=["fire", "fighting", "fairy", "rock"]
+red_pokemon=["Charizard", "Machamp", "Clefable", "Rampardos"]
+blue_elements=["water", "psychic", "dark", "flying", "ice"]
+blue_pokemon=["Blastoise", "Mew", "Darkrai", "Tornadus", "Eiscue"]
+green_elements=["grass", "bug", "steel", "poison", "normal"]
+green_pokemon=["Bulbasaur", "Butterfree", "Melmetal", "Arbok", "Snorlax"]
+yellow_elements=["electric", "ghost", "dragon", "ground"]
+yellow_pokemon=["Pikachu", "Cofagrigus", "Dragonite", "Groudon"] 
+
+def favorite_element(element):
+    for i in range(0, len(red_elements)):
+        if red_elements[i].title() == element:
+            return red_pokemon[i]
+    for i in range(0, len(blue_elements)):
+        if blue_elements[i].title() == element:
+            return blue_pokemon[i]
+    for i in range(0, len(green_elements)):
+        if green_elements[i].title() == element:
+            return green_pokemon[i]
+    for i in range(0, len(yellow_elements)):
+        if yellow_elements[i].title() == element:
+            return yellow_pokemon[i]     
+
+print(favorite_element("Water"))
